e7b2e6fef8ea039e87a45002631ce64373bb2f93
aman95 <aman1995k@gmail.com>
2020-04-05 00:11:11 +0530

    Add initial message in index

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Aman Kumar
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/aman95/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Know more
         </a>
       </header>
     </div>
